8a0ac0712b9a67e4c49e186eed85cddfa31be9d2
tejasph <tphaterp@gmail.com>
2023-12-04 12:54:35 -0800

    Restructured some folders, commented most of bow.main. Tested .sh file with success.

--- models/args.py
+++ models/args.py
@@ -16,7 +16,7 @@ def get_args():
     parser.add_argument('--imbalance-fix', default='loss_weight', choices=['loss_weight', 'undersampling', 'none'],
                         help='How to deal with the class imbalance in our dataset')
     parser.add_argument('--data-dir',
-                        default=os.path.join(r'C:\Users\jjnunez\PycharmProjects', 'scar_nlp_data', 'data'))
+                        default=os.path.join('data', 'toy_data.csv'))
     parser.add_argument('--results-dir', default=os.path.join(r'C:\Users\jjnunez\PycharmProjects\scar_nlp_psych', 'results'))
     parser.add_argument('--data-version', type=str, default='ppv4',
                         help='Version of preprocessing used for this dataset')
---
+++ test_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Evaluating previously trained models to predict seeing a psychiatrist within 12 months using toy data
+#python -m models.bow --target "dspln_PSYCHIATRY_12" --table "demo" --eval_only --model-file "./results/final_results/dspln_PSYCHIATRY_12/BoW/BoW_20230220-1206_e0.pbz2" --data-dir "./demo" --results-dir "./demo"
+python -m models.bow --target "toy_data.csv" --table "toy_data" --data-dir "./data" --results-dir "./data"
