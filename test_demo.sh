b386e7ec7fe3332727cd4e68f4fd4243233a6d92
tejasph <tphaterp@gmail.com>
2023-12-04 15:48:41 -0800

    updated documentation on .sh file.

--- test_demo.sh
+++ test_demo.sh
@@ -2,4 +2,5 @@
 
 # Evaluating previously trained models to predict seeing a psychiatrist within 12 months using toy data
 #python -m models.bow --target "dspln_PSYCHIATRY_12" --table "demo" --eval_only --model-file "./results/final_results/dspln_PSYCHIATRY_12/BoW/BoW_20230220-1206_e0.pbz2" --data-dir "./demo" --results-dir "./demo"
+# Testing toy_data
 python -m models.bow --target "demo" --eval_only --table "toy_data" --data-dir "./data" --results-dir "./data"
