b32e3671af2b1ba97be5798dd2091cc089bf3c59
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2023-12-12 14:25:53 -0800

    Fix indentation and update comments for class imbalance chunk

--- models/bow/__main__.py
+++ models/bow/__main__.py
@@ -24,34 +24,40 @@ if __name__ == '__main__':
         print(f"Loading and evaluating a {model_name} model")
     else:
         print(f"Training and evaluating a {model_name} model")
-
-    # Loss
-    if eval_only:
+    
+    # This if/elif chunk deals with class imbalances
+    if eval_only:  # Using a pre-trained model rather than training a new model
         class_weight = None
         scar_bow = SCARBoW(args, eval_only)
-    elif config.count_tokens: # counts token/doc instead of training
+
+    elif config.count_tokens:  # Counts token/doc instead of training
         scar_bow = SCARBoW(args, False)
         count_bow_tokens(model_name, config, scar_bow)
-        sys.exit()
+        sys.exit()  # Exit the script after counting tokens
+
     elif config.imbalance_fix == 'loss_weight':
         class_weight = 'balanced'
-
+        
+        # Gbdt does not use class_weight
         if config.classifier == "gbdt":
             warnings.warn("sklearn does not have class_weight, so can't use loss_weight to balance"
-                          "gbdt, setting to none", stacklevel=2)
-            # Set config's imbalance_fix to none for gbdt, so when results are written out, we see
-            # that loss weighting wasn't used.
+                        " gbdt, setting to none", stacklevel=2)
             config_dict = vars(config)
-            config_dict['imbalance_fix'] = 'none'
+            config_dict['imbalance_fix'] = 'none'  # Set config's imbalance_fix to none for gbdt, so when results are written out, we see
+                                                # that loss weighting wasn't used.
+        
+        scar_bow = SCARBoW(args, eval_only)  # SCARBow args: args.batch_size, args.data_dir, args.target
 
-        scar_bow = SCARBoW(args, eval_only)  # args = args.batch_size, args.data_dir, args.target
     elif args.imbalance_fix == 'undersampling':
         class_weight = None
-        scar_bow = SCARBoW(args, eval_only, undersample=True) # creates separate files for undersampling
+        scar_bow = SCARBoW(args, eval_only, undersample=True)  # Creates separate files for undersampling
+
     elif config.imbalance_fix == 'none':
-        class_weight = None
+        class_weight = None  # No class imbalance handling
+        
     else:
         raise Exception("Invalid method to fix the class imbalance provided, or not yet implemented")
+
 
     # Make directories for results if not already there
     config.results_dir_target = os.path.join(config.results_dir, config.target)  # dir for a targets results
